3f3fa1e0bec89fc60d8cc76188cf26f1df8abd74
Jan T. Sott <jan@idleberg.com>
2017-06-29 18:34:30 +0200

    add gulp-svg2png

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ const gulp = require('gulp');
 const debug = require('gulp-debug');
 const tslint = require('gulp-tslint');
 const jsonlint = require('gulp-jsonlint');
+const svg2png = require('gulp-svg2png');
 const xmlVal = require('gulp-xml-validator');
 
 // Supported files
@@ -26,6 +27,10 @@ const jsonFiles = [
 
 const xmlFiles = [
   'syntaxes/*.tmLanguage'
+];
+
+const svgFiles = [
+  'src/*.svg'
 ];
 
 // Lint JavaScript
@@ -57,7 +62,18 @@ gulp.task('lint:xml', gulp.series( (done) => {
   done();
 }));
 
+// Convert SVG
+gulp.task('convert:svg', gulp.series( (done) => { 
+  gulp.src(svgFiles)
+    .pipe(svg2png({width: 128, height: 128}))
+    .pipe(gulp.dest('./images'));
+  done();
+}));
+
 // Available tasks
-gulp.task('lint', gulp.parallel('lint:ts', 'lint:json', 'lint:xml',  (done) => {
+gulp.task('lint', gulp.parallel('lint:ts', 'lint:json', 'lint:xml', (done) => {
+  done();
+}));
+gulp.task('build', gulp.parallel('convert:svg', (done) => {
   done();
 }));
