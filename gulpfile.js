5290921dfd109346c9f705dac521d45bbe9606c2
idleberg <jan@idleberg.com>
2018-02-08 17:17:53 +0100

    remove tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,81 +1,17 @@
- /*
- * vscode-gulpfile.js
- *
- * Copyright (c) 2016, 2017 Jan T. Sott
- * Licensed under the MIT license.
- */
-
  // Dependencies
 const gulp = require('gulp');
-const debug = require('gulp-debug');
-const tslint = require('gulp-tslint');
-const jsonlint = require('gulp-jsonlint');
 const raster = require('gulp-raster');
 const rename = require('gulp-rename');
-const xmlVal = require('gulp-xml-validator');
-
-// Supported files
-const tsFiles = [
-  'src/*.ts',
-];
-
-const jsonFiles = [
-  'package.json',
-  'snippets/*.json',
-  'tsconfig.json',
-  'tslint.json'
-];
-
-const xmlFiles = [
-  'syntaxes/*.tmLanguage'
-];
 
 const svgFiles = [
   'node_modules/nsis-logo-v3/src/Logo/outlines-light.svg'
 ];
 
-// Lint TypeScript
-gulp.task('lint:ts', gulp.series( (done) => {
-  gulp.src(tsFiles)
-    .pipe(debug({title: 'tslint'}))
-    .pipe(tslint({
-        formatter: "prose"
-    }))
-    .pipe(tslint.report())
-  done();
-}));
-
-// Lint JSON
-gulp.task('lint:json', gulp.series( (done) => {
-  gulp.src(jsonFiles)
-    .pipe(debug({title: 'json-lint'}))
-    .pipe(jsonlint())
-    .pipe(jsonlint.failAfterError())
-    .pipe(jsonlint.reporter());
-  done();
-}));
-
-// Validate XML
-gulp.task('lint:xml', gulp.series( (done) => {
-  gulp.src(xmlFiles)
-    .pipe(debug({title: 'xml-validator'}))
-    .pipe(xmlVal());
-  done();
-}));
-
 // Convert SVG
-gulp.task('convert:svg', gulp.series( (done) => {
+gulp.task('default',(done) => {
   gulp.src(svgFiles)
     .pipe(raster())
     .pipe(rename("logo.png"))
     .pipe(gulp.dest('./images'));
   done();
-}));
-
-// Available tasks
-gulp.task('lint', gulp.parallel('lint:ts', 'lint:json', 'lint:xml', (done) => {
-  done();
-}));
-gulp.task('build', gulp.parallel('convert:svg', (done) => {
-  done();
-}));
+});
