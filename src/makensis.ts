5283d6596eda8e5af93406f50a894e0ecfaba756
idleberg <jan@idleberg.com>
2018-02-16 17:39:11 +0100

    fix copy&paste mistake

--- src/makensis.ts
+++ src/makensis.ts
@@ -1,179 +1,175 @@
 'use strict';
 
-import { window, workspace, WorkspaceConfiguration } from 'vscode';
+import { window, WorkspaceConfiguration } from 'vscode';
 
-import * as opn from 'opn';
-import { basename, dirname, extname, join } from 'path';
-import { existsSync } from 'fs';
+import * as makensis from 'makensis';
+import { clearOutput, detectOutfile, getConfig, getMakensisPath, getPrefix, isWindowsCompatible, openURL, pathWarning, runInstaller, sanitize } from './util';
 import { platform } from 'os';
 import { spawn } from 'child_process';
 
-const clearOutput = (channel): void => {
-  let config: WorkspaceConfiguration = getConfig();
+const nsisChannel = window.createOutputChannel('NSIS');
 
-  channel.clear();
-  if (config.alwaysShowOutput === true) {
-    channel.show(true);
+const compile = (strictMode: boolean): void => {
+  clearOutput(nsisChannel);
+
+  if (window.activeTextEditor['_documentData']['_languageId'] !== 'nsis') {
+    nsisChannel.appendLine('This command is only available for NSIS files');
+    return;
   }
-};
 
-const detectOutfile = (line): string => {
-  if (line.indexOf('Output: "') !== -1) {
-    let regex = /Output: \"(.*\.exe)\"\r?\n/g;
-    let result = regex.exec(line.toString());
-    if (typeof result === 'object') {
-      try {
-        return (existsSync(result['1']) === true) ? result['1'] : '';
-      } catch (e) {
-        return '';
+  let doc = window.activeTextEditor.document;
+
+  doc.save().then( () => {
+    getMakensisPath()
+    .then(sanitize)
+    .then( (pathToMakensis: string) => {
+      let prefix: string = getPrefix();
+      let config: WorkspaceConfiguration = getConfig();
+
+      let compilerArguments: Array<string>;
+      if (typeof config.compilerArguments !== 'undefined' && config.compilerArguments !== null) {
+        compilerArguments = config.compilerArguments.trim().split(' ');
+      } else {
+        // no default value, since prefixes are OS dependent
+        compilerArguments = [ `${prefix}V4` ];
       }
-    }
-  }
 
-  return '';
-};
-
-const getConfig = (): WorkspaceConfiguration => {
-  return workspace.getConfiguration('nsis');
-};
-
-const getPrefix = (): string => {
-  if (platform() === 'win32') {
-    return '/';
-  }
-
-  return '-';
-};
-
-const isWindowsCompatible = (): boolean => {
-  let config: WorkspaceConfiguration = getConfig();
-
-  if (platform() === 'win32' || config.useWineToRun === true) {
-    return true;
-  }
-
-  return false;
-};
-
-const getMakensisPath = (): Promise<any> => {
-  return new Promise((resolve, reject) => {
-    let pathToMakensis: string = getConfig().pathToMakensis;
-
-    if (typeof pathToMakensis !== 'undefined' && pathToMakensis !== null) {
-      console.log('Using makensis path found in user settings: ' + pathToMakensis);
-      return resolve(pathToMakensis);
-    }
-
-    let which = spawn(this.which(), ['makensis']);
-
-    which.stdout.on('data', (data) => {
-      console.log('Using makensis path detected on file system: ' + data);
-      return resolve(data);
-    });
-
-    which.on('close', (code) => {
-      if (code !== 0) {
-        return reject(code);
+      // only add WX flag if not already specified
+      if (strictMode === true && compilerArguments.indexOf(prefix + 'WX') === -1) {
+        compilerArguments.push(`${prefix}WX`);
       }
-    });
+      compilerArguments.push(doc.fileName);
+
+      // Let's build
+      const child = spawn(pathToMakensis, compilerArguments);
+
+      let stdErr: string = '';
+      let outFile: string = '';
+      let hasWarning: boolean = false;
+
+      child.stdout.on('data', (line: Array<string> ) => {
+        // Detect warnings
+        if (line.indexOf('warning: ') !== -1) {
+          hasWarning = true;
+        }
+        if (isWindowsCompatible() === true && outFile === '') {
+          outFile = detectOutfile(line);
+        }
+
+        nsisChannel.appendLine(line.toString());
+      });
+
+      child.stderr.on('data', (line: Array<any>) => {
+        stdErr += '\n' + line;
+        nsisChannel.appendLine(line.toString());
+      });
+
+      child.on('close', (code) => {
+        let openButton = (isWindowsCompatible() === true && outFile !== '') ? 'Run' : null;
+        if (code === 0) {
+          if (hasWarning === true) {
+            if (config.showNotifications) {
+              window.showWarningMessage(`Compiled with warnings -- ${doc.fileName}`, openButton)
+              .then((choice) => {
+                if (choice === 'Run') {
+                  runInstaller(outFile);
+                }
+              });
+            }
+            if (stdErr.length > 0) console.warn(stdErr);
+          } else {
+            if (config.showNotifications) {
+              window.showInformationMessage(`Compiled successfully -- ${doc.fileName}`, openButton)
+              .then((choice) => {
+                if (choice === 'Run') {
+                  runInstaller(outFile);
+                }
+              });
+            }
+          }
+        } else {
+          nsisChannel.show(true);
+          if (config.showNotifications) window.showErrorMessage('Compilation failed, see output for details');
+          if (stdErr.length > 0) console.error(stdErr);
+        }
+      });
+    })
+    .catch(pathWarning);
   });
 };
 
-const openURL = (cmd: string): void => {
-  opn(`https://idleberg.github.io/NSIS.docset/Contents/Resources/Documents/html/Reference/${cmd}.html`);
-}
-
-const pathWarning = (): any => {
-  window.showWarningMessage('makensis is not installed or missing in your PATH environmental variable', 'Download', 'Help')
-  .then((choice) => {
-    switch (choice) {
-      case 'Download':
-        return opn('https://sourceforge.net/projects/nsis/');
-      case 'Help':
-        return opn('http://superuser.com/a/284351/195953');
-    }
-  });
+const printFlags = (output: string, showFlagsAsObject: boolean = true): void => {
+  if (showFlagsAsObject === true) {
+    nsisChannel.append(JSON.stringify(output, null, 2));
+  } else {
+    nsisChannel.append(output);
+  }
 };
 
-const runInstaller = (outFile): void => {
+const showVersion = (): void => {
   let config: WorkspaceConfiguration = getConfig();
 
-  if (platform() === 'win32') {
-    spawn(outFile);
-  } else if (config.useWineToRun === true) {
-    spawn('wine', [ outFile ]);
-  }
-};
+  clearOutput(nsisChannel);
 
-const sanitize = (response: Object): string => {
-  return response.toString().trim();
-};
-
-const successBridleNsis = (choice): void => {
-  let doc = window.activeTextEditor.document;
-
-  if (choice === 'Open') {
-    let dirName = dirname(doc.fileName);
-    let extName = extname(doc.fileName);
-    let baseName = basename(doc.fileName, extName);
-
-    // because BridleNSIS is kinda buggy
-    let outExt = 'b' + extName.substr(1);
-
-    // if BridleNSIS wasn't buggy
-    // let outExt;
-    // if (extName == '.nsh') {
-    //   outExt = '.bnsh';
-    // } else {
-    //   outExt = '.bnsi';
-    // }
-
-    let outName = baseName + outExt;
-    let nsisFile = join(dirName, outName);
-
-    workspace.openTextDocument(nsisFile)
-    .then( (doc) => {
-      window.showTextDocument(doc);
+  getMakensisPath()
+  .then(sanitize)
+  .then( (pathToMakensis: string) => {
+    makensis.version({pathToMakensis: pathToMakensis})
+    .then(output => {
+      if (config.showVersionAsInfoMessage === true) {
+        window.showInformationMessage(`makensis ${output.stdout} (${pathToMakensis})`);
+      } else {
+        nsisChannel.append(`makensis ${output.stdout} (${pathToMakensis})`);
+      }
+    }).catch(error => {
+      console.error(error);
     });
-  }
+  })
+  .catch(pathWarning);
 };
 
-const successNslAssembler = (choice): void => {
-  let doc = window.activeTextEditor.document;
+const showCompilerFlags = (): void => {
+  let config: WorkspaceConfiguration = getConfig();
 
-  if (choice === 'Open') {
-    let dirName = dirname(doc.fileName);
-    let extName = extname(doc.fileName);
-    let baseName = basename(doc.fileName, extName);
-    let outName = baseName + '.nsi';
-    let nsisFile = join(dirName, outName);
+  clearOutput(nsisChannel);
 
-    workspace.openTextDocument(nsisFile)
-    .then( (doc) => {
-      window.showTextDocument(doc);
+  getMakensisPath()
+  .then(sanitize)
+  .then( (pathToMakensis: string) => {
+    makensis.hdrInfo({pathToMakensis: pathToMakensis, json: config.showFlagsAsObject})
+    .then(output => {
+      printFlags(output.stdout, config.showFlagsAsObject);
+    }).catch(output => {
+      // fallback for legacy NSIS
+      printFlags(output.stdout, config.showFlagsAsObject);
     });
-  }
+  })
+  .catch(pathWarning);
 };
 
-const which = (): string => {
-  if (platform() === 'win32') {
-    return 'where';
-  }
-  return 'which';
+const showHelp = (): void => {
+  let config: WorkspaceConfiguration = getConfig();
+
+  clearOutput(nsisChannel);
+
+  getMakensisPath()
+  .then(sanitize)
+  .then( (pathToMakensis: string) => {
+    makensis.cmdHelp('', {pathToMakensis: pathToMakensis, json: config.showFlagsAsObject})
+    .then(output => {
+      window.showQuickPick(Object.keys(output.stdout)).then(cmd => {
+        if (typeof cmd === 'undefined') {
+          return;
+        }
+
+        openURL(cmd);
+      });
+    }).catch(output => {
+      // fallback for legacy NSIS
+      printFlags(output.stdout, config.showFlagsAsObject);
+    });
+  })
+  .catch(pathWarning);
 };
 
-export {
-  clearOutput,
-  detectOutfile,
-  getConfig,
-  getMakensisPath,
-  getPrefix,
-  isWindowsCompatible,
-  openURL,
-  pathWarning,
-  runInstaller,
-  sanitize,
-  successBridleNsis,
-  successNslAssembler,
-  which
-};
+export { compile, showVersion, showCompilerFlags, showHelp };
