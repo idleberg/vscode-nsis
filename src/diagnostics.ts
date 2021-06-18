0bc4e0a198b6b16623908587a7403bb29cc1da1e
idleberg <git@idleberg.com>
2021-06-19 00:01:35 +0200

    add exclude list for diagnostics

--- src/diagnostics.ts
+++ src/diagnostics.ts
@@ -1,14 +1,28 @@
 import vscode from 'vscode';
+import { basename } from 'path';
 import { compile } from 'makensis';
 import { findErrors, findWarnings, getMakensisPath, getNullDevice, getPreprocessMode, getOverrideCompression, getSpawnEnv } from './util';
 import { getConfig } from 'vscode-get-config';
+import micromatch from 'micromatch';
 
 async function updateDiagnostics(document: vscode.TextDocument | null, collection: vscode.DiagnosticCollection): Promise<void> {
   const { diagnostics } = await getConfig('nsis');
 
-  if (!diagnostics.enabledDiagnostics) return;
+  if (diagnostics.enableDiagnostics !== true) return;
+
+  if (diagnostics.excludedFiles?.length) {
+    const documentBasename = basename(document.fileName);
+    if (micromatch.isMatch(documentBasename, diagnostics.excludedFiles)) {
+      console.log(`Skipping diagnostics for ${document.fileName}, found in exclude list`);
+      collection.clear();
+      return;
+    }
+  }
+
 
   if (document && document.languageId === 'nsis') {
+    console.log(`Running diagnostics for ${document.fileName}`);
+
     let pathToMakensis: string;
 
     try {
