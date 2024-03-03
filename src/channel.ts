5c3676beafaa85b365e5b8eaf420cdce23e8674c
Jan T. Sott <git@idleberg.com>
2024-03-03 17:50:17 +0100

    simplify channel

--- src/channel.ts
+++ src/channel.ts
@@ -1,29 +1,3 @@
 import vscode from 'vscode';
 
-export default {
-  outputChannel: vscode.window.createOutputChannel('NSIS', 'makensis-log'),
-
-  async clear(): Promise<void> {
-    this.outputChannel.clear();
-  },
-
-  dispose(): void {
-    this.outputChannel.dispose();
-  },
-
-  async append(input: unknown): Promise<void> {
-    this.outputChannel.append(input);
-  },
-
-  async appendLine(input: string): Promise<void> {
-    this.outputChannel.appendLine(input);
-  },
-
-  hide(): void {
-    this.outputChannel.hide();
-  },
-
-  show(preserveFocus = true): void {
-    this.outputChannel.show(preserveFocus);
-  }
-}
+export default vscode.window.createOutputChannel('NSIS', 'makensis-log');
