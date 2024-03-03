5fcad47cc0a91f2c1b6138f89847c350f50d2ac0
Jan T. Sott <git@idleberg.com>
2024-03-03 15:29:48 +0100

    add external dependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 	external: ['vscode'],
 	format: 'cjs',
 	minify: true,
-	noExternal: ['dot-prop'],
+	noExternal: ['makensis', 'micromatch', 'open', 'vscode-get-config', 'which'],
 	target: 'es2020',
 	outDir: 'lib',
 });
