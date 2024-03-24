3bd0f00affa092cedd8ccaee688a06b1277fce52
Jan T. Sott <git@idleberg.com>
2024-03-24 14:18:59 +0100

    refactor activate function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { commands,  type ExtensionContext, languages, window, workspace } from 'vscode';
+import { commands, type ExtensionContext, languages, window, workspace } from 'vscode';
 
 // Load package components
 import { compile, showCompilerFlags, showVersion, showHelp } from './makensis';
@@ -11,47 +11,33 @@ export async function activate(context: ExtensionContext): Promise<void> {
 		// TextEditor Commands
 		commands.registerTextEditorCommand('extension.nsis.compile', async () => {
 			return await compile(false);
-		})
-	);
+		}),
 
-	context.subscriptions.push(
 		commands.registerTextEditorCommand('extension.nsis.compile-strict', async () => {
 			return await compile(true);
-		})
-	);
+		}),
 
-	context.subscriptions.push(
 		commands.registerTextEditorCommand('extension.nsis.create-build-task', async () => {
 			return await createTask();
-		})
-	);
+		}),
 
-	context.subscriptions.push(
 		commands.registerTextEditorCommand('extension.nsis.convert-language-file', () => {
 			return convert();
-		})
-	);
+		}),
 
-	// Global Commands
-	context.subscriptions.push(
+		// Global Commands
 		commands.registerCommand('extension.nsis.show-version', async () => {
 			return await showVersion();
-		})
-	);
+		}),
 
-	context.subscriptions.push(
 		commands.registerCommand('extension.nsis.show-compiler-flags', async () => {
 			return await showCompilerFlags();
-		})
-	);
+		}),
 
-	context.subscriptions.push(
 		commands.registerCommand('extension.nsis.command-reference', async () => {
 			return await showHelp();
-		})
-	);
+		}),
 
-	context.subscriptions.push(
 		commands.registerCommand('extension.nsis.open-settings', async () => {
 			commands.executeCommand('workbench.action.openSettings', '@ext:idleberg.nsis');
 		})
@@ -70,21 +56,23 @@ export async function activate(context: ExtensionContext): Promise<void> {
 		}
 	}
 
-	context.subscriptions.push(workspace.onDidSaveTextDocument(() => {
-		const editor = window.activeTextEditor;
+	context.subscriptions.push(
+		workspace.onDidSaveTextDocument(() => {
+			const editor = window.activeTextEditor;
 
-		if (editor) {
-			const document = editor.document || null;
+			if (editor) {
+				const document = editor.document || null;
 
-			updateDiagnostics(document, collection);
-		}
-	}));
+				updateDiagnostics(document, collection);
+			}
+		}),
 
-	context.subscriptions.push(window.onDidChangeActiveTextEditor(editor => {
-		if (editor) {
-			const document = editor.document || null;
+		window.onDidChangeActiveTextEditor(editor => {
+			if (editor) {
+				const document = editor.document || null;
 
-			updateDiagnostics(document, collection);
-		}
-	}));
+				updateDiagnostics(document, collection);
+			}
+		})
+	);
 }
