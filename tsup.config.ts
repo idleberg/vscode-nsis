db87f23ecc9efe6aa5d4432c07d98aefe1cb6002
Jan T. Sott <git@idleberg.com>
2024-12-06 06:00:46 +0100

    sort

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
 	bundle: true,
+	cjsInterop: true,
 	clean: true,
 	entry: ['src/index.ts'],
 	external: ['vscode'],
 	format: 'cjs',
 	minify: true,
 	noExternal: ['@nsis/nlf', 'makensis', 'micromatch', 'open', 'vscode-get-config', 'which'],
-	target: 'es2020',
 	outDir: 'lib',
-	treeshake: true,
-	cjsInterop: true,
 	platform: 'node',
+	target: 'es2020',
+	treeshake: true,
 });
