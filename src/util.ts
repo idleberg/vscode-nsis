06cec239d3fb02c6ff9b42bfbd11c6c27390bdcf
Jan T. Sott <git@idleberg.com>
2025-03-28 22:59:33 +0100

    add comments

--- src/util.ts
+++ src/util.ts
@@ -283,8 +283,12 @@ export async function getSpawnEnv(): Promise<SpawnOptions> {
 		env: {
 			// start with calling process env (this provides e.g. %PATH% on Windows)
 			...process.env,
+
+			// NSIS related
 			NSISDIR: integrated.env[mappedPlatform].NSISDIR || process.env.NSISDIR || undefined,
 			NSISCONFDIR: integrated.env[mappedPlatform].NSISCONFDIR || process.env.NSISCONFDIR || undefined,
+
+			// language settings, fixes occasional issues on macOS and Linux
 			LANG: !isWindows() && !process.env.LANGUAGE ? 'en_US.UTF-8' : undefined,
 			LANGUAGE: !isWindows() && !process.env.LANGUAGE ? 'en_US.UTF-8' : undefined,
 			LC_ALL: !isWindows() && !process.env.LC_ALL ? 'en_US.UTF-8' : undefined,
