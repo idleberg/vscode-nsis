8946221c923899de0275a9e5a23e4a9bacb5fcca
Jan T. Sott <git@idleberg.com>
2025-06-02 22:20:00 +0200

    chore: switch to tsdown as bundler

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,6 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-	bundle: true,
-	cjsInterop: true,
 	clean: true,
 	entry: ['src/index.ts'],
 	external: ['vscode'],
